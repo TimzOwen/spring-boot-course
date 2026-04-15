0d06430f92e02f0f70c8d6f87dc04a4b3cdf61ff
timzowen <timzowen@gmail.com>
2026-04-16 00:01:00 +0300

    initial set up

--- src/main/java/com/timzowen/student/demo/service/StudentService.java
+++ src/main/java/com/timzowen/student/demo/service/StudentService.java
@@ -1,51 +1,26 @@
 package com.timzowen.student.demo.service;
 
 import com.timzowen.student.demo.model.Student;
-import com.timzowen.student.demo.repository.StudentRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class StudentService {
+public interface StudentService {
 
-    private final StudentRepository studentRepository;
+    Student saveStudent(Student student);
 
-    public StudentService(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
+    List<Student> saveAllStudents(List<Student> students);
 
-    public Student saveStudent(Student student) {
-        return studentRepository.save(student);
-    }
+    List<Student> getAllStudents();
 
-    public List<Student> saveAllStudents(List<Student> students) {
-        return studentRepository.saveAll(students);
-    }
+    Student getStudentById(Long id);
 
-    public Student getStudentById(Long id) {
-        return studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found"));
-    }
+    String deleteStudent(Long id);
 
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
-    }
+    String deleteAllStudents();
 
-    public String deleteStudent(Long id) {
-        studentRepository.deleteById(id);
-        return ("Student with " + id + " deleted successfully");
-    }
+    Student updateStudent(Long id, Student student);
 
-    public String deleteAllStudents() {
-        studentRepository.deleteAll();
-        return ("All students deleted successfully");
-    }
+    List<Student> getStudentsByGrade(String grade);
 
-    public Student updateStudent(Long id, Student updatedStudent) {
-        Student existingStudent = getStudentById(id);
-        existingStudent.setName(updatedStudent.getName());
-        existingStudent.setGrade(updatedStudent.getGrade());
-        return studentRepository.save(existingStudent);
-    }
 
 }
